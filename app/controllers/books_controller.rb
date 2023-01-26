bc5f2abef35a21c97649386d8d7ce943f33103e8
kaitoppo <122006842+kaitoppo@users.noreply.github.com>
2023-01-26 12:16:52 +0000

    [中断]課題3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,13 +4,16 @@ class BooksController < ApplicationController
   end
 
   def create
-    @book = Book.new(bokk_params)
+    @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to book_path
+   #redirect_to book_path(@book.id)
+    redirect_to books_path
   end
-  
+
   def index
+    @books_all = Book.all
+    @book = Book.new
   end
 
   def show
@@ -18,4 +21,11 @@ class BooksController < ApplicationController
 
   def edit
   end
+
+private
+
+def book_params
+  params.require(:book).permit(:title, :body)
+end
+
 end
